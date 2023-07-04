e7c377e1845527a27a1b31307411a5840ff9821e
Yashvanth-2272 <138372527+Yashvanth-2272@users.noreply.github.com>
2023-07-04 11:17:40 +0530

    Update Equipment.java

--- src/main/java/com/hackathon/FarmToMarket/entity/Equipment.java
+++ src/main/java/com/hackathon/FarmToMarket/entity/Equipment.java
@@ -1,5 +1,29 @@
 package com.hackathon.FarmToMarket.entity;
 
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Equipment {
+	@Id
+	private int id;
+	private String equipName;
+	private String location;
+	private int count;
+	private String Status;
+	@OneToMany
+	@JoinColumn(name = "userId")
+	private User user;
 
 }
+
