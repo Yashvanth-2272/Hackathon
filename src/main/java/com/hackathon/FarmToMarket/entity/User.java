70192efee7a5b334f07cee448f066d0a2fd774b6
Yashvanth-2272 <138372527+Yashvanth-2272@users.noreply.github.com>
2023-07-04 11:16:52 +0530

    Update User.java

--- src/main/java/com/hackathon/FarmToMarket/entity/User.java
+++ src/main/java/com/hackathon/FarmToMarket/entity/User.java
@@ -2,7 +2,23 @@ package com.hackathon.FarmToMarket.entity;
 
 import javax.persistence.Entity;
 
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+	@Id
+	private int userId;
+	private String name;
+	private String type;
+	private String email;
+	private Long phnNo;
 
 }
